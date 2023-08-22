6027a05a7d37afb1ba1e36474fb8bd9ddbbf03b8
Artyom Rodionov <artemr19032006@gmail.com>
2023-08-22 15:52:06 +0300

    First commit to recipe adding feature branch

--- feature/recipe_adding/presentation/src/main/java/com/recipe_adding/presentation/screens/recipe_adding/RecipeAddingScreenFragment.kt
+++ feature/recipe_adding/presentation/src/main/java/com/recipe_adding/presentation/screens/recipe_adding/RecipeAddingScreenFragment.kt
@@ -5,19 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.recipe_adding.presentation.databinding.FragmentRecipeAddingBinding
+import com.recipe_adding.presentation.databinding.FragmentRecipeAddingScreenBinding
 
-class RecipeAddingFragment : Fragment() {
+class RecipeAddingScreenFragment : Fragment() {
 
-    private var _binding: FragmentRecipeAddingBinding? = null
+    private var _binding: FragmentRecipeAddingScreenBinding? = null
 
-    private val binding: FragmentRecipeAddingBinding get() = _binding!!
+    private val binding: FragmentRecipeAddingScreenBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRecipeAddingBinding.inflate(inflater, container, false)
+        _binding = FragmentRecipeAddingScreenBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
---
+++ feature/recipe_adding/presentation/src/main/java/com/recipe_adding/presentation/screens/recipe_adding/RecipeAddingScreenViewModel.kt
@@ -0,0 +1,5 @@
+package com.recipe_adding.presentation.screens.recipe_adding
+
+import androidx.lifecycle.ViewModel
+
+class RecipeAddingScreenViewModel : ViewModel()
